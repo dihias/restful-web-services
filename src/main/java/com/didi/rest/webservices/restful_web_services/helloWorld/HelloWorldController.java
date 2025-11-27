63cc887d3bf435d30e4e94db202ce5af442012a4
dihia <dihiasalhi@gmail.com>
2025-11-27 13:59:33 -0500

    feat: add path variable support to Hello World endpoint
    Introduce a dynamic endpoint that accepts a value directly from the URL

--- src/main/java/com/didi/rest/webservices/restful_web_services/helloWorld/HelloWorldController.java
+++ src/main/java/com/didi/rest/webservices/restful_web_services/helloWorld/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.didi.rest.webservices.restful_web_services.helloWorld;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class HelloWorldController {
     @GetMapping("hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean("Hello World " + name);
     }
 }
