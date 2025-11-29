b68b909d6695f5ae16ad4bb95aeddcc94a78bf70
dihia <dihiasalhi@gmail.com>
2025-11-29 18:24:56 -0500

    feat: Versioning REST API - Request Parameter Versioning

--- src/main/java/com/didi/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ src/main/java/com/didi/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -5,13 +5,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
-    @GetMapping("v1/person")
+    @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson()
     {
         return new PersonV1 ("Bob Marley");
     }
-    @GetMapping("v2/person")
+    @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson()
+    {
+        return new PersonV2(new Name("Bob","Marley"));
+    }
+
+    @GetMapping(path="/person", params= "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter()
+    {
+        return new PersonV1("Bob Marley");
+    }
+
+    @GetMapping(path="/person", params= "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter()
     {
         return new PersonV2(new Name("Bob","Marley"));
     }
