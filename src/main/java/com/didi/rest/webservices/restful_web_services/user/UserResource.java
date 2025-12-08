1d23acd0f0ace2b9c59ac68fbea503451553b82e
dihia <dihiasalhi@gmail.com>
2025-12-07 20:35:33 -0500

    fixing errors and connecting to H2 database

--- src/main/java/com/didi/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/didi/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,16 +1,12 @@
 package com.didi.rest.webservices.restful_web_services.user;
 
 import jakarta.validation.Valid;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UserResource {
@@ -27,17 +23,9 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public EntityModel<User> FindUser(@PathVariable int id)
+    public ResponseEntity<User> FindUser(@PathVariable int id)
     {
-        User user = userDaoService.findUser(id);
-        if(user == null)
-            throw new UserNotFoundException(" id "+ id);
-
-        EntityModel<User> entityModel = EntityModel.of(user);
-        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).FindAllUsers());
-        entityModel.add(link.withRel("all-users"));
-        return entityModel;
-
+        return ResponseEntity.ok(userDaoService.findUser(id));
     }
 
     @PostMapping("/users")
--- src/test/java/com/didi/rest/webservices/restful_web_services/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/didi/rest/webservices/restful_web_services/RestfulWebServicesApplicationTests.java
@@ -3,7 +3,7 @@ package com.didi.rest.webservices.restful_web_services;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RestfulWebServicesApplication.class)
 class RestfulWebServicesApplicationTests {
 
 	@Test
